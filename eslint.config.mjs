49d24ef9c3298f76c08557c5ca49ad5cbb3fbd2e
Marcos Melo <marcosvnmelo.dev@gmail.com>
2026-04-18 12:23:45 -0400

    chore(eslint): enable some rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,18 @@ import { defineConfig } from 'eslint/config';
 import ts from 'typescript-eslint';
 
 export default defineConfig([
+  {
+    ignores: ['**/dist', '**/.rslib', '**/*.config.ts', '**/*.config.mjs'],
+  },
   js.configs.recommended,
   ts.configs.recommended,
-  { ignores: ['**/dist', '**/.rslib'] },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.eslint.json'],
+      },
+    },
+  },
   {
     files: ['scripts/**/*.mjs'],
     languageOptions: {
@@ -21,18 +30,18 @@ export default defineConfig([
   {
     files: ['**/src/**/*.ts', '**/e2e/**/*.ts'],
     rules: {
+      '@typescript-eslint/consistent-type-definitions': 'error',
+      '@typescript-eslint/consistent-type-imports': 'warn',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'error',
       '@typescript-eslint/no-unsafe-function-type': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-require-imports': 'off',
-      '@typescript-eslint/no-namespace': 'off',
-      '@typescript-eslint/consistent-type-imports': 'warn',
-      '@typescript-eslint/consistent-type-definitions': 'error',
     },
   },
   {
     files: ['**/*.spec.ts'],
     rules: {
+      '@typescript-eslint/no-floating-promises': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
     },
   },
